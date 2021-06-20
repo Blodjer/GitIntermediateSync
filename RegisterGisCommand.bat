9271e1dcd4cf242666c3a9825c0cc1852b4eff2f
Blodjer <j95.szwaczka@gmail.com>
2021-06-20 17:07:36 +0200

    Added message to register command script

--- RegisterGisCommand.bat
+++ RegisterGisCommand.bat
@@ -1,1 +1,6 @@
+@echo off
 Powershell -Command "& { $key = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment', $true); $path = $key.GetValue('Path',$null,'DoNotExpandEnvironmentNames'); $key.SetValue('Path', $path + ';%%OneDriveConsumer%%\!sync\git;', 'ExpandString'); $key.Dispose(); }"
+echo.
+echo Command registered. Please restart the computer to complete the setup!
+echo.
+pause
